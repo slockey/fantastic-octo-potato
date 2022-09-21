51642cc0bb5c4a8be32e822eeaf00d672d4dad70
Steven Lockey <steven.lockey@cision.com>
2022-09-21 10:49:30 -0400

    Generated base project files based on spring initializer + base project files added + updated gitignore to cover intellij, maven, java

---
+++ src/main/java/com/slockey/fantasticoctopotato/FantasticOctoPotatoApplication.java
@@ -0,0 +1,13 @@
+package com.slockey.fantasticoctopotato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasticOctoPotatoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FantasticOctoPotatoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/slockey/fantasticoctopotato/FantasticOctoPotatoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slockey.fantasticoctopotato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasticOctoPotatoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
